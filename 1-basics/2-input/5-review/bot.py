4221fd8c2b221608edb52bb0fa8fc4b0540944a9
parxhall <4hallp41@solent.ac.uk>
2019-09-24 16:35:30 +0000

    combined all the techniques i have learnt

---
+++ 1-basics/2-input/5-review/bot.py
@@ -0,0 +1,19 @@
+print("Hello human, what is your name?")
+name = str(input())
+print("""I'm Beep and i am so expressionate that we can be friends,""", name, """.
+Anyway I need you're help. Could you tell me how many eyes I have?""")
+eyes = int(input())
+print("using the letter pad infront of you could you choose my eyes")
+eye = input()
+print("Great how about the number of cracks or broken parts i have?")
+cracks = int(input())
+
+#diplays outputs
+print(" _","♦"*cracks,"________")
+print("|  _____  |")
+print("| |_"+eye*eyes+"_"+eye*eyes+"_| |")
+print("\\_\\_____/_/")
+print("  |  o  |")
+print(" (|  o  |)")
+print("  \\  o  /")
+print("   \\___/")
