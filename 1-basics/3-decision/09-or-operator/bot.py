63dffaaf03e636f00dbe131c2eaf6ee11d5feb44
parxhall <4hallp41@solent.ac.uk>
2019-10-01 10:18:12 +0000

    Added code to demonstrate the use of a logical and operator.

--- 1-basics/3-decision/09-or-operator/bot.py
+++ 1-basics/3-decision/09-or-operator/bot.py
@@ -4,7 +4,7 @@ adventure = input("What type of adventure should I have?\n")
 if (adventure == "scary") or (adventure == "short"):
     print("Entering the dark forest!")
 #else if
-elif (adventure == "safe") or (adventure == "long"):
+elif adventure == "safe" or "long":
     print("Taking the safe route!")
 #else
 else:
