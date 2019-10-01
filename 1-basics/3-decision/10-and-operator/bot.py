aa872eb51a64a4e4c0f8ba05a3c2e7ed42ec9deb
parxhall <4hallp41@solent.ac.uk>
2019-10-01 10:15:50 +0000

    Added code to demonstrate the use of a logical and operator.

---
+++ 1-basics/3-decision/10-and-operator/bot.py
@@ -0,0 +1,9 @@
+#ask for input
+hear = input("What did I hear?\n")
+see = input("What did I see?\n")
+#if statement
+if (hear == "grr") and (see == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+#else
+else:
+    print("I am a little scared but I will continue.")
