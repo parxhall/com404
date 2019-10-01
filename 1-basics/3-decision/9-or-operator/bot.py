bae311175204bdff9258934863e0bc3f77c0f1c2
parxhall <4hallp41@solent.ac.uk>
2019-10-01 10:09:40 +0000

    Added code to demonstrate the use of a logical or operator.

---
+++ 1-basics/3-decision/9-or-operator/bot.py
@@ -0,0 +1,11 @@
+#ask for input
+adventure = input("What type of adventure should I have?\n")
+#if statement
+if (adventure == "scary") or (adventure == "short"):
+    print("Entering the dark forest!")
+#else if
+elif (adventure == "safe") or (adventure == "long"):
+    print("Taking the safe route!")
+#else
+else:
+    print("Not sure which route to take.")
