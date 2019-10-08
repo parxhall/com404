80a994f47bcbe3f529c3a46be6e2acffb0c1f4c7
parxhall <4hallp41@solent.ac.uk>
2019-10-08 09:00:12 +0000

    demonstrating addition within a while loop

---
+++ 1-basics/4-repetition/1-while-loop/5-sum-100/bot.py
@@ -0,0 +1,14 @@
+#variables
+count = 0
+total = 0
+
+
+print("Calculating the sum of the first 100 numbers...")
+
+#while input is more than one print until equation is disproved
+while count < 100:
+    count += 1
+    total += count
+
+#finish statement once while loop is complete
+print("Done! The answer is ", str(total))
