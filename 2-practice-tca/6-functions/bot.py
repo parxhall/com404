42be848ee9fa87006ad1c5c28bc101d398d446d6
parxhall <4hallp41@solent.ac.uk>
2019-10-22 09:04:41 +0000

    functions

--- 2-practice-tca/6-functions/bot.py
+++ 2-practice-tca/6-functions/bot.py
@@ -1,13 +1,12 @@
 #functions
 def is_league_united(hero_1,hero_2):
     if hero_1 == "superman" and hero_2 == "wonder woman" or hero_1 == "wonder woman" and hero_2 == "superman":
-        print("True")
+        return("True")
     else:
-        print("false")
+        return("false")
 
 def decide_plan(hero_1,hero_2):
-    is_league_united(hero_1,hero_2)
-    if is_league_united == "True":
+    if is_league_united(hero_1,hero_2) == "True":
         print("Time to save the world!")
     else:
         print("We must unite the league!")
@@ -17,7 +16,7 @@ def run():
     hero_2 = input("What is our second heros name?\n")
     option = input("What should we do? (plan or league)\n")
     if option == "league":
-        is_league_united(hero_1,hero_2)
+        print(is_league_united(hero_1,hero_2))
     elif option == "plan":
         decide_plan(hero_1,hero_2)
     else:
